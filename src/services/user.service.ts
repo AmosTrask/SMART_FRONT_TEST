f5961857985a1f161589953d18af840b1acdcb47
Vincent Redouté <vincent.redoute@live.fr>
2018-04-14 18:34:00 +0200

    getting back old getUser

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,7 +1,7 @@
 import {HttpClient} from "@angular/common/http";
 import {Injectable} from "@angular/core";
 import {Observable} from "rxjs/Observable";
-import {User} from "../Entity/user";
+import {User} from "../entities/user";
 import {API_SERVER} from "../app/app.constants";
 import {of} from 'rxjs/observable/of';
 import {catchError} from "rxjs/operators";
@@ -21,21 +21,16 @@ export class UserService {
       .pipe(catchError(this.handleError('getAllUsers', [])));
   }
 
-  getUser(): Observable<User[]>{
-    const url = `${API_SERVER.user}`;
-    return this.http.get<User[]>(url)
-      .pipe(catchError(this.handleError('getAllUsers', [])));
-  }
 
   /**
    * Get the current user
    * @returns {Observable<User>}
    */
-  /*getUser(): Observable<User>{
+  getUser(): Observable<User>{
     const url = `${API_SERVER.user}`;
     return this.http.get<User>(url)
       .pipe(catchError(this.handleError<User>('getUser')));
-  }*/
+  }
 
   /**
    * Handle Http operation that failed.
