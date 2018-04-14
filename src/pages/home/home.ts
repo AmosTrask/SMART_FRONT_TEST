29aba5f830fb4b840ac89f54b4a76277c2a10fde
Vincent Redouté <vincent.redoute@live.fr>
2018-04-14 18:34:42 +0200

    login from home with alert if failed

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,38 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import {AlertController} from 'ionic-angular';
+import {LoginInfos} from "../../entities/loginInfos";
+import {LoginService} from "../../services/login.service";
+import {User} from "../../entities/user";
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
+
 export class HomePage {
 
+  loginInfos : LoginInfos = {
+    username: "",
+    password: ""
+  }
+  user: User;
 
-  constructor(public navCtrl: NavController) {
+  constructor(public alertCtrl: AlertController, private loginService: LoginService) {
     localStorage.clear();
+  }
+
+  login() {
+    this.loginService.login(this.loginInfos)
+      .subscribe((user) => {
+        this.user = user;
+    }, (err) => {
+      console.error(err);
+      let alert = this.alertCtrl.create({
+            title: 'Authentification failed!',
+            subTitle: 'Wrong username or password!',
+            buttons: ['OK']
+          });
+          alert.present();
+    });
   }
 }
--- src/services/login.service.ts
+++ src/services/login.service.ts
@@ -1,13 +1,10 @@
 import { Injectable } from '@angular/core';
 import {HttpClient, HttpHeaders} from '@angular/common/http';
 import {AuthService} from './auth.service';
-import {LoginInfos} from "../Entity/loginInfos";
+import {LoginInfos} from "../entities/loginInfos";
 import {API_SERVER} from "../app/app.constants";
-import {of} from "rxjs/observable/of";
-import {Observable} from "rxjs/Observable";
-import {catchError} from "rxjs/operators";
 import "rxjs/add/operator/do";
-import {User} from '../Entity/user';
+
 
 
 const httpOptions = {
@@ -19,40 +16,21 @@ export class LoginService {
 
   constructor(private http: HttpClient, private authService: AuthService) { }
 
-
-  login (loginInfos: LoginInfos): Observable<User> {
+  login (loginInfos: LoginInfos) {
     const url = API_SERVER.auth;
-    return this.http.post<User>(url, loginInfos, httpOptions)
-      .do(res => this.setSession(res))
-      .pipe(catchError(this.handleError<User>('login'))
-      );
-  }
-
-  private setSession(authResult) {
-    console.log(authResult.token);
-    this.authService.setToken(authResult.token);
-    this.authService.setLogged(true);
+    return this.http.post<any>(url, loginInfos, httpOptions)
+      .map(res => {
+        if(res.token) {
+          this.authService.setToken(res.token);
+          this.authService.setLogged(true);
+        }
+        // Return res for the moment
+        return res;
+      });
   }
 
   logout() {
     this.authService.setToken(null);
     this.authService.setLogged(false);
-  }
-
-
-  /**
-   * Handle Http operation that failed.
-   * Let the app continue.
-   * @param operation - name of the operation that failed
-   * @param result - optional value to return as the observable result
-   */
-  private handleError<T> (operation = 'operation', result?: T) {
-    return (error: any): Observable<T> => {
-
-      console.error(error); // log to console for the moment
-
-      // Let the app keep running by returning an empty result.
-      return of(result as T);
-    };
   }
 }
