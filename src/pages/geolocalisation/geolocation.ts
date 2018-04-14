4fe5f6fcc70ebd93e2fb95e3c1928d605a3c8204
Vincent Redouté <vincent.redoute@live.fr>
2018-04-14 17:35:29 +0200

    no more auto login in geolocation

--- src/pages/geolocalisation/geolocation.ts
+++ src/pages/geolocalisation/geolocation.ts
@@ -1,9 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { Geolocation } from '@ionic-native/geolocation';
-import {LoginInfos} from "../../Entity/loginInfos";
-import {User} from "../../Entity/user";
-import {LoginService} from "../../services/login.service";
 
 @Component({
   selector: 'page-geolocation',
@@ -13,17 +10,8 @@ export class GeolocationPage {
   lat: any;
   lng: any;
 
-  loginInfos: LoginInfos = {
-    username : 'admin',
-    password : 'admin'
-  };
+  constructor(public navCtrl: NavController, private geolocation: Geolocation) { }
 
-  connectedUser: User;
-
-
-  constructor(public navCtrl: NavController, private geolocation: Geolocation, private loginService: LoginService) {
-
-  }
 
   ionViewDidLoad(){
     this.geolocation.getCurrentPosition().then((resp) => {
@@ -32,12 +20,5 @@ export class GeolocationPage {
     }).catch((error) => {
       console.log('Error getting location', error);
     });
-
-    this.login();
-  }
-
-
-  login() : void {
-    this.loginService.login(this.loginInfos).subscribe(user => this.connectedUser = user);
   }
 }
