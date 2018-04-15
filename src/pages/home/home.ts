cd1832231f848004eeb2134768d089b72577acba
Vincent Redouté <vincent.redoute@live.fr>
2018-04-15 12:17:57 +0200

    displaying logged when logged

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -3,6 +3,8 @@ import {AlertController} from 'ionic-angular';
 import {LoginInfos} from "../../entities/loginInfos";
 import {LoginService} from "../../services/login.service";
 import {User} from "../../entities/user";
+import {UserService} from "../../services/user.service";
+import {AuthService} from "../../services/auth.service";
 
 @Component({
   selector: 'page-home',
@@ -14,18 +16,27 @@ export class HomePage {
   loginInfos : LoginInfos = {
     username: "",
     password: ""
-  }
+  };
+
   user: User;
 
-  constructor(public alertCtrl: AlertController, private loginService: LoginService) {
-    localStorage.clear();
+  logged = false;
+
+  constructor(public alertCtrl: AlertController, private loginService: LoginService, private userService: UserService,
+              private authService: AuthService)
+  { }
+
+  ionViewDidLoad () {
+    this.logged = this.authService.isLogged();
   }
 
   login() {
     this.loginService.login(this.loginInfos)
-      .subscribe((user) => {
-        this.user = user;
-    }, (err) => {
+      .subscribe(() => {
+        this.getUser();
+        this.logged = this.authService.isLogged();
+        },
+        (err) => {
       console.error(err);
       let alert = this.alertCtrl.create({
             title: 'Authentification failed!',
@@ -34,5 +45,19 @@ export class HomePage {
           });
           alert.present();
     });
+  }
+
+  getUser(): void {
+    this.userService.getUser()
+      .subscribe(user => { this.user = user; },
+        (err) => {
+          console.error(err);
+          let alert = this.alertCtrl.create({
+            title: 'Request failed!',
+            subTitle: 'You must be authenticated',
+            buttons: ['OK']
+          });
+          alert.present();
+        });
   }
 }
