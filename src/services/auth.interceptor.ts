3e00672820068f4edd9b9e84b3a9dff2b8bcc297
Vincent Redouté <vincent.redoute@live.fr>
2018-04-15 19:17:34 +0200

    remove console log

--- src/services/auth.interceptor.ts
+++ src/services/auth.interceptor.ts
@@ -9,7 +9,6 @@ export class AuthInterceptor implements HttpInterceptor {
     constructor(private authService: AuthService) { }
 
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-      console.log(JSON.stringify(req));
 
     const token: string = this.authService.getToken();
 
