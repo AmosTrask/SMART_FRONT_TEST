754cdf06504a315e8449baa57496736b836520b1
Vincent Redouté <vincent.redoute@live.fr>
2018-04-15 12:17:42 +0200

    removing getUser from users page

--- src/pages/users/users.ts
+++ src/pages/users/users.ts
@@ -12,28 +12,11 @@ export class UsersPage {
 
   users: User [];
 
-  connectedUser: User;
-
-    constructor(public alertCtrl: AlertController, private userService: UserService) {
+  constructor(public alertCtrl: AlertController, private userService: UserService) {
      this.getAllUsers();
-     this.getUser();
   }
 
   getAllUsers(): void {
     this.userService.getAllUsers().subscribe(users => this.users = users );
-  }
-
-  getUser(): void {
-    this.userService.getUser()
-      .subscribe(user => { this.connectedUser = user },
-        (err) => {
-          console.error(err);
-          let alert = this.alertCtrl.create({
-            title: 'Request failed!',
-            subTitle: 'You must be authenticated',
-            buttons: ['OK']
-          });
-          alert.present();
-        });
   }
 }
